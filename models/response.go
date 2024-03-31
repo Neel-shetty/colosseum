a86d283e5d51d3c6cd6a780cd3842fa791b30f01
Neel <neelnarayanshetty@gmail.com>
2024-03-31 23:58:05 +0530

    Updates

--- models/response.go
+++ models/response.go
@@ -3,11 +3,11 @@ package models
 import "github.com/gofiber/fiber/v2"
 
 type HttpError struct {
-	status  string
+	status  string `validate:"oneof=fail success"`
 	message string
 }
 
 type HttpSuccess struct {
-	status string
+	status string `validate:"oneof=fail success"`
 	data   fiber.Map
 }
