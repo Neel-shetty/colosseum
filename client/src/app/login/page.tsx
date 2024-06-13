45fb757f42b7ca63b774dc3ff61e20557d23093f
Margaret LJ <margaretlj04@gmail.com>
2024-06-13 18:08:48 +0530

    signup

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -118,7 +118,7 @@ export default function Login() {
 
           <div className="mt-4 text-center text-sm text-white">
             Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
+            <Link href="/signup" className="underline">
               Sign up
             </Link>
           </div>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Login from "./login/page";
+
 export default function Home() {
   return (
     <>
     <Login/>
    <Link href="./pages/leaderboard">go to leaderboard</Link>
+   
     </>
    
   );
