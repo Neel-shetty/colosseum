0b099e94a98d793a779702f8eb83c37f7f372494
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-02-24 22:37:26 +0530

    actions go backend

--- backend/models/user.go
+++ backend/models/user.go
@@ -11,25 +11,26 @@ import (
 
 // @Schema
 type User struct {
-	ID               uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
-	Name             string         `gorm:"varchar(255);not null" json:"name"`
-	Email            string         `gorm:"not null;unique;index:" json:"email"`
-	Password         string         `gorm:"not null" json:"password"`
-	PhoneNumber      int            `gorm:"not null" json:"phoneNumber"`
-	Score            int            `gorm:"not null;default:1000" json:"score"`
-	CreatedAt        time.Time      `gorm:"not null" json:"createdAt,omitempty"`
-	UpdatedAt        time.Time      `gorm:"not null" json:"updatedAt,omitempty"`
-	ProfilePic       sql.NullString `json:"profilPic"`
-	Ban              bool           `gorm:"default:0" json:"ban"`
-	MonkeyTypeApiKey sql.NullString `gorm:"column:monkey_type_api_key" json:"monkeyTypeApiKey"`
-	GithubUsername   sql.NullString `json:"githubUsername"`
+	ID                   uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
+	Name                 string         `gorm:"varchar(255);not null" json:"name"`
+	Email                string         `gorm:"not null;unique;index:" json:"email"`
+	Password             string         `gorm:"not null" json:"password"`
+	PhoneNumber          int            `gorm:"not null" json:"phoneNumber"`
+	Score                int            `gorm:"not null;default:1000" json:"score"`
+	CreatedAt            time.Time      `gorm:"not null" json:"createdAt,omitempty"`
+	UpdatedAt            time.Time      `gorm:"not null" json:"updatedAt,omitempty"`
+	ProfilePic           sql.NullString `json:"profilPic"`
+	Ban                  bool           `gorm:"default:0" json:"ban"`
+	MonkeyTypeApiKey     sql.NullString `gorm:"column:monkey_type_api_key" json:"monkeyTypeApiKey"`
+	GithubUsername       sql.NullString `json:"githubUsername"`
 	MonkeyTypeProfileUrl sql.NullString `json:"monkeyTypeProfileUrl"`
-	Skills           pq.StringArray `gorm:"type:text[]" json:"skills"`
-	Branch           string         `json:"branch"`
-	Year             int            `gorm:"not null;default:1" json:"year"`
-	About            string         `json:"about"`
-	ShowOnBoarding   bool           `gorm:"not null;default:true" json:"showOnBoarding"`
-	MonkeyTypeScore  int            `gorm:"not null;default:0" json:"monkeyTypeScore"`
+	InstrgramUsername    sql.NullString `json:"instrgramUsername"`
+	Skills               pq.StringArray `gorm:"type:text[]" json:"skills"`
+	Branch               string         `json:"branch"`
+	Year                 int            `gorm:"not null;default:1" json:"year"`
+	About                string         `json:"about"`
+	ShowOnBoarding       bool           `gorm:"not null;default:true" json:"showOnBoarding"`
+	MonkeyTypeScore      int            `gorm:"not null;default:0" json:"monkeyTypeScore"`
 }
 
 type CreateUserSchema struct {
@@ -41,7 +42,6 @@ type CreateUserSchema struct {
 	Branch      string         `json:"branch"`
 	Year        int            `json:"year"`
 	About       string         `json:"about"`
-	
 }
 
 type UpdateUserSchema struct {
@@ -54,7 +54,7 @@ type UpdateUserSchema struct {
 	Branch           string         `json:"branch"`
 	Year             int            `json:"year"`
 	About            string         `json:"about"`
-	ProfilePic	   string         `json:"profilePic"`
+	ProfilePic       string         `json:"profilePic"`
 }
 
 type GetUserSchema struct {
